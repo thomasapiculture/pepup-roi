75fe42a00475a8269985ccae0d24f2f051b2c879
thomasapiculture <valentin.blanchet@thomas-apiculture.com>
2026-09-24 18:42:12 +0200

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = 'pepup-roi-v1';
+const CACHE = 'pepup-roi-v3';
 const FILES = ['./', './index.html', './manifest.json'];
 
 self.addEventListener('install', e => {
@@ -15,10 +15,10 @@ self.addEventListener('activate', e => {
 
 self.addEventListener('fetch', e => {
   e.respondWith(
-    caches.match(e.request).then(r => r || fetch(e.request).then(res => {
+    fetch(e.request).then(res => {
       const clone = res.clone();
       caches.open(CACHE).then(c => c.put(e.request, clone));
       return res;
-    })).catch(() => caches.match('./index.html'))
+    }).catch(() => caches.match(e.request))
   );
 });
